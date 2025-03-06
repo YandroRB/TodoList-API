a9f1a2bc197fdec8c74b9dc46e6e9e47d5bcfb95
YandroRB <santiago.rdbn@outlook.com>
2025-03-06 10:38:58 -0500

    API TodoList v0.1 rollback 2

--- src/main/java/com/todolist/todo/exception/RolExistenteException.java
+++
@@ -1,7 +0,0 @@
-package com.todolist.todo.exception;
-
-public class RolExistenteException extends RuntimeException {
-    public RolExistenteException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/todolist/todo/exception/RolNoExistenteException.java
+++
@@ -1,7 +0,0 @@
-package com.todolist.todo.exception;
-
-public class RolNoExistenteException extends RuntimeException {
-    public RolNoExistenteException(String message) {
-        super(message);
-    }
-}
